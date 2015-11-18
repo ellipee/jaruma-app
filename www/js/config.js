4ef00fa8af23767d0b4ecffc895fcd6318529857
Codrick <isaac@mibstechnologies.com>
2015-11-18 08:27:46 +0100

    <commint  message>

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'http://jaruma.net/n/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://jaruma.net/n/push/')
+.constant('GCM_SENDER_ID', '729148524569')
+
+;
